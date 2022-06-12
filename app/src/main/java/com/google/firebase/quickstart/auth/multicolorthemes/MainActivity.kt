8d950cc59a98a81f487a83a3d815dee50904b847
Abaynhe Abitew <abaynheabitew@yahoo.com>
2022-06-12 06:01:57 -0500

    A better way to have multi them. But the code is outdated and need more imporovement

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/multicolorthemes/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.google.firebase.quickstart.auth.multicolorthemes
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val blue_theme_btn : Button = findViewById(R.id.blue_theme_btn)
+        val red_theme_btn : Button = findViewById(R.id.red_theme_btn)
+
+        blue_theme_btn.setOnClickListener {
+
+            Utility.setTheme(applicationContext,1)
+            recreateActivity()
+        }
+        red_theme_btn.setOnClickListener {
+            Utility.setTheme(applicationContext,2)
+            recreateActivity()
+        }
+
+
+    }
+
+
+   /* fun onClick(v: View) {
+        when (v.getId()) {
+            R.id.blue_theme_btn -> {
+                Utility.setTheme(
+                    applicationContext, 1
+                )
+                recreateActivity()
+            }
+            R.id.red_theme_btn -> {
+                Utility.setTheme(
+                    applicationContext, 2
+                )
+                recreateActivity()
+            }
+        }
+    }*/
+
+    fun recreateActivity() {
+        val intent = intent
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        finish()
+        overridePendingTransition(0, 0)
+        startActivity(intent)
+        overridePendingTransition(0, 0)
+    }
+}
